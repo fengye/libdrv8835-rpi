b6588f0f5e7bc274d4b7415c0b7dc4dd6465afa1
Ye Feng <feng_ye2001@hotmail.com>
2020-05-26 16:50:24 +1200

    Make shared library

--- drv8835.h
+++ drv8835.h
@@ -9,20 +9,22 @@
 #include <memory.h>
 #include "types.h"
 
+#define EXPORT __attribute__ ((visibility("default")))
+
 #ifdef RASPI
-extern result_t	drv8835_server_init();
-extern result_t	drv8835_server_listen(int port);
-extern result_t	drv8835_server_quit();
-extern result_t	drv8835_server_wait_till_quit();
-extern bool		drv8835_server_is_connected();
-extern result_t	drv8835_server_force_quit();
-extern result_t	drv8835_server_set_motor_param(uint8_t motor, int16_t param);
-extern result_t	drv8835_server_set_motor_params(uint8_t motor1, int16_t param1, uint8_t motor2, int16_t param2);
+extern EXPORT result_t	drv8835_server_init();
+extern EXPORT result_t	drv8835_server_listen(int port);
+extern EXPORT result_t	drv8835_server_quit();
+extern EXPORT result_t	drv8835_server_wait_till_quit();
+extern EXPORT bool		drv8835_server_is_connected();
+extern EXPORT result_t	drv8835_server_force_quit();
+extern EXPORT result_t	drv8835_server_set_motor_param(uint8_t motor, int16_t param);
+extern EXPORT result_t	drv8835_server_set_motor_params(uint8_t motor1, int16_t param1, uint8_t motor2, int16_t param2);
 #endif
 
-extern result_t	drv8835_client_connect(const char* host, int port);
-extern result_t	drv8835_client_disconnect();
-extern bool		drv8835_client_is_connected();
-extern result_t	drv8835_client_send_motor_param(uint8_t motor, int16_t param);
-extern result_t	drv8835_client_send_motor_params(uint8_t motor1, int16_t param1, uint8_t motor2, int16_t param2);
+extern EXPORT result_t	drv8835_client_connect(const char* host, int port);
+extern EXPORT result_t	drv8835_client_disconnect();
+extern EXPORT bool		drv8835_client_is_connected();
+extern EXPORT result_t	drv8835_client_send_motor_param(uint8_t motor, int16_t param);
+extern EXPORT result_t	drv8835_client_send_motor_params(uint8_t motor1, int16_t param1, uint8_t motor2, int16_t param2);
 
